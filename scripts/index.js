9d15206af70232b97e2ccf5309c2162bdf1e39f2
KunalK20102 <63280440+KunalK20102@users.noreply.github.com>
2020-08-31 21:47:07 -0700

    udate

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '1 sep, 2020',
+  birthdate: '2 sep, 2020',
   name: 'Priya ji'
 };
 
