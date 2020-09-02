a07434dfc09f238f6574058bb4ae32d9c2eca3db
KunalK20102 <63280440+KunalK20102@users.noreply.github.com>
2020-09-02 14:09:57 +0530

    upodate

--- firework js.js
+++ firework js.js
@@ -90,10 +90,6 @@ var TxtType = function(el, toRotate, period) {
 // finsh firework1
 
 // cake bLLON NAME STERT
- window.open = function() {};
-            window.print = function() {};
-            // Support hover state for mobile.
-            if (false) {
-                window.ontouchstart = function() {};
+
             }
 // CAKE BALLON NAMD]=E FINISH
